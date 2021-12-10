005c6a176e08c386a0600424c34f61a2ada4d28f
southorange <southorange1228@gmail.com>
2021-12-10 15:52:09 +0800

    feat: temp

--- packages/ping/lib/ping.cpp
+++ packages/ping/lib/ping.cpp
@@ -1,11 +1,11 @@
 #include "ping.h"
+#include "netdb.h"
 
 using namespace std;
 
 Napi::Object Ping::Init(Napi::Env env, Napi::Object exports) {
     Napi::Function func = DefineClass(env,"Ping",{
         InstanceMethod("start",&Ping::start),
-        InstanceMethod("end",&Ping::end)
     });
 
     Napi::FunctionReference *constructor = new Napi::FunctionReference();
@@ -18,6 +18,7 @@ Napi::Object Ping::Init(Napi::Env env, Napi::Object exports) {
 
 Ping::Ping(const Napi::CallbackInfo &info): Napi::ObjectWrap<Ping>(info) {
     Napi::Env env = info.Env();
+    this->env = env;
 
     int length = info.Length();
 
@@ -31,7 +32,8 @@ Ping::Ping(const Napi::CallbackInfo &info): Napi::ObjectWrap<Ping>(info) {
     }
     Napi::Object obj = info[0].ToObject();
     if (obj.Has("addr") && obj.Get("addr").IsString()){
-        this->options.addr = obj.Get("addr").As<Napi::String>();
+        string originAddr = obj.Get("addr").As<Napi::String>();
+        this->options.addr = (char *)originAddr.c_str();
     }else{
         Napi::TypeError::New(env,"addr should be string").ThrowAsJavaScriptException();
         return;
@@ -49,7 +51,101 @@ Ping::Ping(const Napi::CallbackInfo &info): Napi::ObjectWrap<Ping>(info) {
             Napi::TypeError::New(env,"retry should be number").ThrowAsJavaScriptException();
             return;
         }
+    }else{
+        this->options.timeout = 60 * 1000;
     }
+    this->send_pack_num = 0;
+    this->recv_pack_num = 0;
+    this->lost_pack_num = 0;
+
+    this->min_time = 0;
+    this->max_time = 0;
+    this->sum_time = 0;
+
+}
+
+Ping::~Ping() {
+    if(close(sock_fd) == -1) {
+        fprintf(stderr, "Close socket error:%s \n\a", strerror(errno));
+        Napi::TypeError::New(env,"Close socket Error").ThrowAsJavaScriptException();
+        exit(1);
+    }
+}
+
+unsigned short Ping::CheckSum(unsigned short * header, int length) {
+    int check_sum = 0;              //校验和
+    int nleft = length;          //还未计算校验和的数据长度
+    unsigned short * p = header; //用来做临时指针
+    unsigned short temp;            //用来处理字节长度为奇数的情况
+
+    while(nleft > 1){
+        check_sum += *p++;          //check_sum先加以后，p的指针才向后移
+        nleft -= 2;
+    }
+
+    //奇数个长度
+    if(nleft == 1){
+        //利用char类型是8个字节，将剩下的一个字节压入unsigned short（16字节）的高八位
+        *(unsigned char *)&temp = *(unsigned char *)p;
+        check_sum += temp;
+    }
+
+    check_sum = (check_sum >> 16) + (check_sum & 0xffff);   //将之前计算结果的高16位和低16位相加
+    check_sum += (check_sum >> 16);                         //防止上一步也出现溢出
+    temp = ~check_sum;              //temp是最后的校验和
+
+    return temp;
+}
+
+void Ping::CreateSocket() {
+    struct protoent * protocol;             //获取协议用
+    unsigned long in_addr;                  //用来保存网络字节序的二进制地址
+    struct hostent host_info, * host_pointer; //用于gethostbyname_r存放IP信息
+    char buff[2048];                         //gethostbyname_r函数临时的缓冲区，用来存储过程中的各种信息
+    int errnop = 0;                         //gethostbyname_r函数存储错误码
+
+    //通过协议名称获取协议编号
+    if((protocol = getprotobyname("icmp")) == NULL){
+        fprintf(stderr, "Get protocol error:%s \n\a", strerror(errno));
+        exit(1);
+    }
+
+    //创建原始套接字，这里需要root权限，申请完成之后应该降权处理
+    if((sock_fd = socket(AF_INET, SOCK_RAW, protocol->p_proto)) == -1){
+        fprintf(stderr, "Greate RAW socket error:%s \n\a", strerror(errno));
+        exit(1);
+    }
+
+    //降权处理，使该进程的EUID，SUID的值变成RUID的值
+    setuid(getuid());
+
+    //设置send_addr结构体
+    send_addr.sin_family = AF_INET;
+
+    //判断用户输入的点分十进制的ip地址还是域名，如果是域名则将其转化为ip地址，并备份
+    //inet_addr()将一个点分十进制的IP转换成一个长整数型数
+    if((in_addr = inet_addr(input_domain.c_str())) == INADDR_NONE){
+        //输入的不是点分十进制的ip地址
+        if(gethostbyname_r(input_domain.c_str(), &host_info, buff, sizeof(buff), &host_pointer, &errnop)){
+            //非法域名
+            fprintf(stderr, "Get host by name error:%s \n\a", strerror(errno));
+            exit(1);
+        } else{
+            //输入的是域名
+            this->send_addr.sin_addr = *((struct in_addr *)host_pointer->h_addr);
+        }
+    } else{
+        //输入的是点分十进制的地址
+        this->send_addr.sin_addr.s_addr = in_addr;
+    }
+
+    //将ip地址备份下来
+    this->backup_ip = inet_ntoa(send_addr.sin_addr);
+
+    printf("PING %s (%s) %d(%d) bytes of data.\n", input_domain.c_str(),
+           backup_ip.c_str(), PACK_SIZE - 8, PACK_SIZE + 20);
+
+    gettimeofday(&first_send_time, NULL);
 }
 
 Napi::Value Ping::start(const Napi::CallbackInfo &info) {
@@ -57,7 +153,3 @@ Napi::Value Ping::start(const Napi::CallbackInfo &info) {
     return Napi::Number::New(env,100);
 }
 
-Napi::Value Ping::end(const Napi::CallbackInfo &info)  {
-    Napi::Env env = info.Env();
-    return Napi::String::New(env,"hello world");
-}
--- packages/ping/lib/ping.h
+++ packages/ping/lib/ping.h
@@ -1,21 +1,73 @@
 #include "napi.h"
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string>
+#include <string.h>
+#include <netinet/ip_icmp.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
 
 using namespace std;
 
 struct PingOptions {
-    string addr;
+    char * addr;
     int timeout;
     int retry;
 };
+
+#define PACK_SIZE 32;
 
 class Ping: public Napi::ObjectWrap<Ping> {
 public:
     static Napi::Object Init(Napi::Env env, Napi::Object exports);
     Ping(const Napi::CallbackInfo &info);
+    ~Ping();
 
 private:
     Napi::Value start(const Napi::CallbackInfo &info);
-    Napi::Value end(const Napi::CallbackInfo &info);
+
+    // 校验和
+    unsigned short CheckSum(unsigned short *header,int length);
+    void CreateSocket();
+
+    void SendPacket();
+    void RecvPacket();
+
+    void statistic();
+
+    int GeneratePacket();
+    int ResolvePakcet(int pack_szie);
 
     PingOptions options;
+
+    std::string input_domain;       //用来存储通过main函数的参数传入的域名或者ip
+    std::string backup_ip;          //通过输入的域名或者ip转化成为的ip备份
+
+    int sock_fd;
+
+    int max_wait_time;              //最大等待时间
+
+    int send_pack_num;              //发送的数据包数量
+    int recv_pack_num;              //收到的数据包数量
+    int lost_pack_num;              //丢失的数据包数量
+
+    struct sockaddr_in send_addr;   //发送到目标的套接字结构体
+    struct sockaddr_in recv_addr;   //接受来自目标的套接字结构体
+
+    char* send_pack;      //用于保存发送的ICMP包
+    char* recv_pack;      //用于保存接收的ICMP包
+
+    struct timeval first_send_time; //第一次发送ICMP数据包时的UNIX时间戳
+    struct timeval recv_time;       //接收ICMP数据包时的UNIX时间戳
+
+    double min_time;
+    double max_time;
+    double sum_time;
+
+    Napi::Env env = NULL;
 };
